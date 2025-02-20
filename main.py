173f81717f7b41cb880ffd5cdb765aa6541a01bc
Rohini-guvi <pkrd.rohini89@gmail.com>
2025-02-20 12:59:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ st.title("Cricsheet Match Data Analysis ")
 
 tc = TableCreation()
 
-# # #Calling Match_Dataframe function to Create tables for IPL,ODIs,Test Series and T20s
-# tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/ipl_json/','ipl')
-# tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/odis_json/','odis')
-# tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/t20s_json/','t20s')
-# tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/tests_json/','testseries')
+# Calling Match_Dataframe function to Create tables for IPL,ODIs,Test Series and T20s
+tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/ipl_json/','ipl')
+tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/odis_json/','odis')
+tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/t20s_json/','t20s')
+tc.match_dataframe('C:/Users/HP/OneDrive/Project2-Cricsheet/tests_json/','testseries')
 
 current_page=st.navigation([st.Page("ViewTable.py"),
                             st.Page("Query.py")])
